323169ba4d7f25e9c4fe51b7dc2fe06daf869cbb
synthezzz <pan.bogdan3301@gmail.com>
2021-01-12 13:56:59 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Sample text on feature branch
+          Sample text on dev
         </p>
       </div>
     );
